0cffd25acc2c64518800a0ff86b50f3f35ee0113
sorabhjainnitj <sorabhjainnitj@gmail.com>
2018-11-10 11:15:09 +0530

    Setting up some issues regarding footer and active menu problem. Make front page little responsive.

--- FrontEnd/src/java/FinalYearProject/Frontend/controller/PageController.java
+++ FrontEnd/src/java/FinalYearProject/Frontend/controller/PageController.java
@@ -15,7 +15,26 @@ public class PageController {
    public ModelAndView index()
    {   ModelAndView mv= new ModelAndView("index");
       System.out.println("server request mapped to controller");          
-      mv.addObject("username","hello agar chala to party");
+      mv.addObject("title","Home");
+      mv.addObject("UserPressedHome","true");
 	  return mv;
    }
+   @RequestMapping(value= {"/about"})
+   public ModelAndView about()
+   {   ModelAndView mv= new ModelAndView("index");
+      System.out.println("server request mapped to controller");          
+      mv.addObject("title","About Us");
+      mv.addObject("UserPressedAbout","true");
+	  return mv;
+   }
+ 
+    @RequestMapping(value ={"/contactus"})
+    public ModelAndView ContactUs()
+    {  ModelAndView mv= new ModelAndView("index");
+        System.out.println("server request mapped to controller");          
+      mv.addObject("title","Contact Us");
+      mv.addObject("UserPressedContactUs","true");
+	  return mv;
+        
+    }
  }
